26671f508acbcd3aa87dd1a1e19144d5257d2d2d
DhanushM12 <dhanushmukhi@gmail.com>
2023-08-29 22:26:33 +0530

    create and get all

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const Student = require('../models/student');
+
+// create a student
+router.post('/create', async (req, res) => {
+    const student = new Student({
+        name: req.body.name,
+        college: req.body.college
+    })
+    try {
+        const newStudent = await student.save();
+        res.status(201).json(newStudent);
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+// get all data
+router.get('/list', async (req, res) => {
+    try{
+        const students = await Student.find({});
+        res.status(200).json(students)
+    } catch (error){
+        return res.status(500).json({message: error.message})
+    }
+})
+
+module.exports = router;
